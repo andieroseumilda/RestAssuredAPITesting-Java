d0a7a9ad44d5d8d5754cd116c61523dea48a3ea9
Andielyne Umilda <andielyne.umilda@directwithhotels.com>
2018-09-11 08:33:55 +0800

    logged in

--- src/test/java/framework/TestAPI.java
+++ src/test/java/framework/TestAPI.java
@@ -3,11 +3,14 @@ package framework;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
 
 public class TestAPI {
 
     @Test
     public void redirectToCpPage(){
-        given().get("https://staging-cp.directwithhotels.com/login").then().log().all();
+        given().formParam("user_name","sp.andie")
+                
+                .post("https://staging-cp.directwithhotels.com/login/process_login").body().print();
     }
 }
